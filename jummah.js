a86c9dbdeed8665c8e60a32dde446c2de769b1a6
Ithris Shahulhameed <99539135+Ithris-IFT@users.noreply.github.com>
2023-05-14 13:33:51 +0530

    Add files via upload

---
+++ jummah.js
@@ -0,0 +1,173 @@
+// Get the button:
+let mybutton = document.getElementById("myBtntop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+// Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAKc72biSrwu8AndfjL3CVCPu9q-65bEVU",
+    authDomain: "jummah-70c65.firebaseapp.com",
+    projectId: "jummah-70c65",
+  databaseURL: "    https://jummah-70c65-default-rtdb.firebaseio.com/",
+    storageBucket: "jummah-70c65.appspot.com",
+    messagingSenderId: "79719605617",
+    appId: "1:79719605617:web:b21b6ca95d718d627582d0"
+  };
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var contactformDB=firebase.database().ref("DHAYINAMES");
+document.getElementById('form').addEventListener('submit',validate);
+
+
+document.getElementById("add").addEventListener("click",function(event){event.preventDefault()}); 
+const table=document.getElementById("tableBody");
+var datetime=new Date().toString(); 
+
+ function validate() {
+
+  // body...
+
+var name=document.getElementById('name');
+
+const save= (name,datetime)=>{
+  var newform=contactformDB.push();
+  newform.set({
+    dhayiname :name.value,
+    datetime:datetime
+  });
+  };
+if(name.value===""){
+  alert("Please Enter Dhayi Name")}
+else{
+
+ let pwd=prompt("Please Enter Password To Add dhayiName");
+      //confirmation password
+      if (pwd!= "tntj"){
+        
+        if (confirm("ℹ️INCORRECT PASSWORD❗  \n Press 'OK' to TRY AGAIN 🔄...")) {
+          validate();
+        }else{
+          alert("If You Want To Add dhayiname,Please Enter The Correct Password...");
+          name.value='';
+        }
+        
+      }else{
+        alert("_AUTHENTICATED SUCCESSFULLY_✔️")
+       save(name,datetime);
+       setTimeout(()=>{alert("Added successfully....")},100);
+
+       document.getElementById('added_message').style.display="block";
+       setTimeout(()=>{document.getElementById('added_message').style.display = "none" ;},3000);
+
+       name.value='';}
+     }
+   }
+  //      document.getElementById('added_message').style.display="block";
+  //     setTimeout(()=>{document.getElementById('added_message').style.display = "none" ;},4500);
+  // save(name,datetime);
+  // name.value='';}}
+
+//read data in database(firebase)
+contactformDB.on('value',(snapshot)=>{
+  var i=1;
+   table.innerHTML ="";
+  snapshot.forEach((usersdata)=>{
+    key = usersdata.key;
+    uservalue=usersdata.val();
+    document.getElementById("fetch").style.display="none";
+     table.innerHTML +=`<tr>
+     <td>${i}</td><td>${uservalue.dhayiname}</td>
+     <td onclick="removeData('${this.key}','${this.uservalue.dhayiname}')" id="del_btn">Delete
+     </tr>`;
+ document.getElementById("total").innerHTML=i;
+
+    i++;
+// console.log(key)
+  })
+})
+// function prompt1(){
+//       let text;
+//       let person=prompt("edit dhayi name :");
+//       if (person==null||person=="")
+//       {
+//         text="";
+        
+//         if (confirm("if u want to edit dhayi  Name 😢")) {
+//           prompt1();
+//         }else{
+//           alert("sorry, for the Trouble ! 🤞🤐");
+//         }
+        
+//       }else{
+//         text="Hello  "+ person +"! successfully edited";
+//         alert(text);
+//       }}
+//  function editData(key){
+//     prompt1();
+//     console.log()
+//    firebase.database().ref('DHAYINAMES/'+key).once('value',(snapshot)=>{
+//    name.value=snapshot.val().dhayiname;
+
+//  })
+//  return false;
+//  }
+// console.log(table)
+function removeData(key,uservalue){
+      console.log()
+       if (confirm("Do You Want To Delete?"+"\n"+uservalue)) {
+         navigator.vibrate([50,100,50]);
+       setTimeout(()=>{alert("Deleted Successfully...")},200);
+
+         
+          document.getElementById('delete_message').style.display="block";
+         setTimeout(()=>{document.getElementById('delete_message').style.display = "none" ;},4500);
+           firebase.database().ref('DHAYINAMES/'+key).remove();
+           return false;
+          
+        }else{
+          // else part
+       // console.log(table)
+
+
+        }
+
+}
+//nav bar responsive
+$(window).on('scroll',function(){
+            if($(window).scrollTop()){
+                $('nav').addClass('black');
+            }
+            else{
+                $('nav').removeClass('black');
+            }
+        })
+  /*menu button onclick function*/         $(document).ready(function(){
+                $('.menu h4').click(function(){
+                    $("nav ul").toggleClass("active")
+            })
+            })
+
+ // feedback confirmation
+  function feedback(){
+       if (confirm("opening Gmail or Email")) {
+          document.getElementById('fb').href ="mailto:ithris615@yahoo.com";
+          navigator.vibrate([50,100,50]);
+          setTimeout(()=>{document.getElementById('fb').href ="#";},2000);
+        }else{document.getElementById('fb').href ="#";navigator.vibrate([100]);}
+}
